711351bc16586b3605f0e280fa360f23a81f94bb
Ian Berigan <43962789+IanB1717@users.noreply.github.com>
2019-02-23 23:12:38 +0000

    Add files via upload

--- AsciiCodeEncoder.java
+++ AsciiCodeEncoder.java
@@ -19,18 +19,18 @@ public class AsciiCodeEncoder{
     for(int r=0;r<len;r++)
     {
       convertBinary(ar[r]);
-    }	
+    }
 	}
 	public static int[] changeAsc(String sent,int len)
 	{
 		int asc[]=new int[len];
     		for(int i = 0; i < len ; i++)
-    	{   // while counting characters if less than the length add one        
+    	{   // while counting characters if less than the length add one
         	char character = sent.charAt(i); // start on the first character
         	int ascii = (int) character; //convert the first character
 			asc[i]=ascii;
 		}
-		
+
 		return asc;
 	}
     public static void convertBinary(int decimal) {
@@ -50,40 +50,48 @@ public class AsciiCodeEncoder{
             decimal=decimal/2;
 
         }
-        System.out.print(binary+" ");
+        if(binary.length()<7)
+        {
+        	System.out.print("0"+binary+" ");
+        }
+        else
+        {
+        	System.out.print(binary+" ");
+        }
+
 
     }
 }
 }
-class NoOfOccurenceOfCharacters { 
-    static final int MAX_CHAR = 256; 
-  
-    static void getOccuringChar(String str) 
-    { 
-        // Create an array of size 256 i.e. ASCII_SIZE 
-        int count[] = new int[MAX_CHAR]; 
-  
-        int len = str.length(); 
-  
-        // Initialize count array index 
-        for (int i = 0; i < len; i++) 
-            count[str.charAt(i)]++; 
-  
-        // Create an array of given String size 
-        char ch[] = new char[str.length()]; 
-        for (int i = 0; i < len; i++) { 
-            ch[i] = str.charAt(i); 
-            int find = 0; 
-            for (int j = 0; j <= i; j++) { 
-  
-                // If any matches found 
-                if (str.charAt(i) == ch[j])  
-                    find++;                 
-            } 
-  
-            if (find == 1)  
-                System.out.println("Number of Occurrence of " + 
-                 str.charAt(i) + " is:" + count[str.charAt(i)]);             
-        } 
-    } 
-} 
+class NoOfOccurenceOfCharacters {
+    static final int MAX_CHAR = 256;
+
+    static void getOccuringChar(String str)
+    {
+        // Create an array of size 256 i.e. ASCII_SIZE
+        int count[] = new int[MAX_CHAR];
+
+        int len = str.length();
+
+        // Initialize count array index
+        for (int i = 0; i < len; i++)
+            count[str.charAt(i)]++;
+
+        // Create an array of given String size
+        char ch[] = new char[str.length()];
+        for (int i = 0; i < len; i++) {
+            ch[i] = str.charAt(i);
+            int find = 0;
+            for (int j = 0; j <= i; j++) {
+
+                // If any matches found
+                if (str.charAt(i) == ch[j])
+                    find++;
+            }
+
+            if (find == 1)
+                System.out.println("Number of Occurrence of " +
+                 str.charAt(i) + " is:" + count[str.charAt(i)]);
+        }
+    }
+}
